5e5aaea2901ec584d10be19e4b7b2ddf5d69b762
Vadim Kravcov <v.sem4ik@mail.ru>
2023-06-22 12:50:27 +0300

    add second func

--- str_func.py
+++ str_func.py
@@ -2,6 +2,6 @@ def foo_up(value):
     return value.upper()
 
 def foo_capitalize(value):
-    """Function that can make your words capitalize"""
+    """Function that can make your words capitalize  """
     return value.capitalize()
 
